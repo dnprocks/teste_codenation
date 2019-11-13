291cb989d56416112e588037479c900619aea7b0
fo005462 <alexsander.wilen@virtualsistemas.com.br>
2019-11-13 17:01:08 -0300

    Commit inicial com a estrutura inicial do projeto.

---
+++ src/test/java/br/com/challenge/CentralDeErrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.challenge.centraldeerros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralDeErrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
